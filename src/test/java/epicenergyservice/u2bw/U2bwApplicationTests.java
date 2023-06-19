4c00679947ae8bc3521aba2a8a007ee5271e2370
12kpro <97319981+12kpro@users.noreply.github.com>
2023-06-19 10:36:23 +0200

    project setup

---
+++ src/test/java/epicenergyservice/u2bw/U2bwApplicationTests.java
@@ -0,0 +1,13 @@
+package epicenergyservice.u2bw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2bwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
