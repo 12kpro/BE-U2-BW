e35c34da0f49b87e944293eb72a822d06987018c
AlfonsoDeBartolo <alfo.deba@gmail.com>
2023-06-19 17:39:41 +0200

    Fatture StatoFatture Repositories Services

--- src/main/java/epicenergyservice/u2bw/fatture/repositories/FatturaRepository.java
+++ src/main/java/epicenergyservice/u2bw/fatture/repositories/FatturaRepository.java
@@ -2,10 +2,23 @@ package epicenergyservice.u2bw.fatture.repositories;
 
 import epicenergyservice.u2bw.fatture.Fattura;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FatturaRepository extends JpaRepository<Fattura, UUID> {
+
+    @Query("SELECT f FROM Fattura f")
+    List<Fattura> findAll();
+
+    @Query("SELECT f FROM Fattura f WHERE f.id = :id")
+    Optional<Fattura> findById(@Param("id") UUID id);
+
+    @Query("SELECT f FROM Fattura f WHERE f.cliente.id = :clienteId")
+    List<Fattura> findByClienteId(@Param("clienteId") UUID clienteId);
 }
